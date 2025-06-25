fb3d598e06005700a69c88dffadc51c988fd3a86
rockysingh728208 <sagarkashyap9155@example.com>
2025-06-26 01:24:33 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
-import connectDb from "./db/index.js";
-
-
-// agar mai ye likhta hoo to package json me add karna hoga "dev":"nodemon ke baad --require dotenv/config  "
-import dotenv from 'dotenv';
-dotenv.config({path:'./.env'});
-
-
-
-
-
-// import dotenv from 'dotenv';
-// dotenv.config({ path: './.env' });
-
-connectDb()
 
 
 // first approach
@@ -38,3 +23,28 @@ connectDb()
 //     throw err
 // }
 // })()
+
+
+
+
+// second approach
+
+import connectDb from "./db/index.js";
+// agar mai ye likhta hoo to package json me add karna hoga "dev":"nodemon ke baad --require dotenv/config  "
+import dotenv from 'dotenv';
+dotenv.config({path:'./.env'});
+
+// import dotenv from 'dotenv';
+// dotenv.config({ path: './.env' });
+
+connectDb()
+.then(() => {
+    app.listen(process.env.PORT||8000, () => {
+        console.log(`App is listening on port ${process.env.PORT }`);
+    })
+})
+.catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
+}
+)
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiResponse{
+    constructor(statusCode,data, message="success") {
+        this.statusCode = statusCode; // HTTP status code
+        this.message = message; // Response message
+        this.data = data; // Optional data payload
+    this.success=statusCode<400
+    }
+}
